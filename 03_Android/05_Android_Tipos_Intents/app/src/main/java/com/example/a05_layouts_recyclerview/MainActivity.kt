7e23a7e2f00d57eaa80fb7df612c3e717a2b0565
jossued <jossuedutan@gmail.com>
2019-06-13 16:03:32 -0500

    RecyclerView-Intents

---
+++ 03_Android/05_Android_Tipos_Intents/app/src/main/java/com/example/a05_layouts_recyclerview/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.a05_layouts_recyclerview
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_ir_a_recycler_view.setOnClickListener {
+            irARecyclerView()
+        }
+
+        btn_enviar_intent.setOnClickListener{
+            irAIntentRespusta()
+        }
+    }
+
+    private fun irARecyclerView(){
+        val intent = Intent(
+            this,
+            ReciclerViewActivity::class.java
+        )
+        startActivity(intent)
+    }
+
+    fun irAIntentRespusta(){
+        val intentExplicito = Intent(
+            this,
+            IntentRespuestaActivity::class.java
+        )
+        startActivity(intentExplicito)
+    }
+}
